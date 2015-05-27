6c603ee2da7badb02256169db98df2d94cb098c5
Luis Fernandez <lfergon@gmail.com>
2015-05-27 17:24:57 +0200

    Export csv first commit

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  summary: "Export a collection to csv",
+  name: "lfergon:exportcsv",
+  version: "0.0.1",
+  git: "https://github.com/lfergon/meteor-export-csv.git"
+});
+
+Package.on_use(function(api) {
+  api.add_files([
+    'lib/exportcsv.js'
+  ], ['client', 'server']);
+  api.add_files([
+    'lib/FileSaver.min.js'
+  ], ['client']);
+  if (api.export) {
+    api.export('exportcsv');
+    api.export('saveAs');
+  }
+});
